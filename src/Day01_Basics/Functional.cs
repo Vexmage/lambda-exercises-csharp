15fe0dd40f902e0346e8e73abfe61985423edeee
Joel Southall <vextechmage@gmail.com>
2025-09-29 05:23:47 -0700

    chore: initial commit with Day01 setup and tests

---
+++ src/Day01_Basics/Functional.cs
@@ -0,0 +1,50 @@
+﻿namespace Day01_Basics;
+
+public static class Functional
+{
+    public static IEnumerable<U> Map<T, U>(IEnumerable<T> source, Func<T, U> f)
+    {
+        foreach (var item in source)
+            yield return f(item);
+    }
+
+    public static IEnumerable<T> Filter<T>(IEnumerable<T> source, Predicate<T> pred)
+    {
+        foreach (var item in source)
+            if (pred(item)) yield return item;
+    }
+
+    // Educational: yields intermediate totals (1, then 3, then 6)
+    public static IEnumerable<T> Reduce<T>(IEnumerable<T> source, Func<T, T, T> combine, T seed)
+    {
+        var acc = seed;
+        foreach (var item in source)
+        {
+            acc = combine(acc, item);
+            yield return acc;
+        }
+    }
+
+    // Loop-capture pitfall
+    public static List<Func<int>> LoopCapturePitfall()
+    {
+        var funcs = new List<Func<int>>();
+        for (int i = 0; i < 3; i++)
+        {
+            funcs.Add(() => i);
+        }
+        return funcs;
+    }
+
+    // Fixed capture using a local copy
+    public static List<Func<int>> LoopCaptureFixed()
+    {
+        var funcs = new List<Func<int>>();
+        for (int i = 0; i < 3; i++)
+        {
+            var copy = i;
+            funcs.Add(() => copy);
+        }
+        return funcs;
+    }
+}
---
+++ tests/Day01_Basics.Tests/MapFilterTests.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;                 // for .Select(...)
+using Day01_Basics;
+using FluentAssertions;
+
+namespace Day01_Basics.Tests;
+
+public class MapFilterTests
+{
+    [Fact]
+    public void Map_AppliesFunctionToEachElement()
+    {
+        var src = new[] { 1, 2, 3 };
+        var doubled = Functional.Map(src, x => x * 2);
+        doubled.Should().Equal(2, 4, 6);
+    }
+
+    [Fact]
+    public void Filter_RemovesNonMatching()
+    {
+        var src = new string?[] { "a", "", "b", null };
+        var nonEmpty = Functional.Filter(src!, s => !string.IsNullOrEmpty(s));
+        nonEmpty.Should().Equal("a", "b");
+    }
+
+    [Fact]
+    public void Reduce_ShowsIntermediateAccumulators()
+    {
+        var src = new[] { 1, 2, 3 };
+        var sums = Functional.Reduce(src, (a, b) => a + b, 0);
+        sums.Should().Equal(1, 3, 6);
+    }
+
+    [Fact]
+    public void LoopCapturePitfall_ShowsSameFinalValue()
+    {
+        var funcs = Functional.LoopCapturePitfall();
+        funcs.Select(f => f()).Should().OnlyContain(x => x == 3);
+    }
+
+    [Fact]
+    public void LoopCaptureFixed_CapturesDistinctValues()
+    {
+        var funcs = Functional.LoopCaptureFixed();
+        funcs.Select(f => f()).Should().Equal(0, 1, 2);
+    }
+}
